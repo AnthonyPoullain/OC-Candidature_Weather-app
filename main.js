0e1c0580d50d7d4cecfcbe1dc74bdfd54b8057ef
Anthony <anthonypoullain.pro@gmail.com>
2021-11-25 18:31:03 +0100

    design improved and invalid city error handled

--- main.js
+++ main.js
@@ -1,9 +1,22 @@
+// DOM ELEMENTS
+const inputEl = document.querySelector("#search-city");
+const searchBtnEl = document.querySelector("#search-btn");
+const locationEL = document.querySelector("#location");
+const tempEl = document.querySelector("#temp");
+const iconEl = document.querySelector("#icon");
+const descriptionEl = document.querySelector("#description");
+const humidityEl = document.querySelector("#humidity");
+const windEl = document.querySelector("#wind");
+const bgEl = document.querySelector("#app-bg");
+const inputErrorEl = document.querySelector("#city-error");
+
 // API INFO
 const api = {
-  key: "4ede1a53b5e91a609e217cb7b55e6366",
-  base: "https://api.openweathermap.org/data/2.5/",
   // Example:
   // api.openweathermap.org/data/2.5/weather?q={city name}&appid={API key}
+
+  key: "4ede1a53b5e91a609e217cb7b55e6366",
+  base: "https://api.openweathermap.org/data/2.5/",
 
   searchApi: async function (city) {
     const response = await fetch(
@@ -13,19 +26,6 @@ const api = {
   },
 };
 
-// DOM ELEMENTS
-const inputEl = document.querySelector("#search-city");
-const searchBtnEl = document.querySelector("#search-btn");
-
-const locationEL = document.querySelector("#location");
-const tempEl = document.querySelector("#temp");
-const iconEl = document.querySelector("#icon");
-const descriptionEl = document.querySelector("#description");
-const humidityEl = document.querySelector("#humidity");
-const windEl = document.querySelector("#wind");
-const bgEl = document.querySelector("#app-bg");
-const typewriterEl = document.querySelector(".typewriter");
-
 // HANDLE EVENTS
 inputEl.addEventListener("keyup", function (event) {
   if (event.keyCode === 13) {
@@ -34,20 +34,30 @@ inputEl.addEventListener("keyup", function (event) {
 });
 
 function resetAnimation() {
-  const el = typewriterEl;
+  const el = locationEL;
   el.style.animation = "none";
   el.offsetHeight; /* trigger reflow */
   el.style.animation = null;
+}
+
+function resetSearchBar() {
+  inputEl.value = "";
+  inputErrorEl.classList.remove("display");
 }
 
 searchBtnEl.addEventListener("click", () => {
   let city = inputEl.value;
 
   api.searchApi(city).then((data) => {
-    if (data.name == undefined) return;
+    if (data.name == undefined) {
+      inputErrorEl.classList.add("display");
+      return;
+    }
+
+    resetAnimation();
+    resetSearchBar();
 
     bgEl.innerHTML = `<img src="https://source.unsplash.com/featured/?${city}" alt="background-city-image"/>`;
-    resetAnimation();
     locationEL.innerText = data.name;
     tempEl.innerHTML = `${Math.round(
       data.main.temp
